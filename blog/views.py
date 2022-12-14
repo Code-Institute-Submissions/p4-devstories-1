e9df7091ab335670640d3042437a7007e76c0872
eoghankb <eoghankb@gmail.com>
2022-12-14 20:50:59 +0000

    added class postlist

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
 
 
-def home(request):
-    return render(request, 'base.html', {})
+class PostList(ListView):
+    model = Post
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "base.html"
+    paginate_by = 6
