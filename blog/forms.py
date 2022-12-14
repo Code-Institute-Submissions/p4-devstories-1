bf0ba60931322e967659d2fab2e475c8082b1692
eoghankb <eoghankb@gmail.com>
2022-12-14 22:22:48 +0000

    added comment form and blog post model form

---
+++ blog/forms.py
@@ -0,0 +1,21 @@
+from .models import Comment, Post
+from django.utils.translation import gettext_lazy as _
+from django import forms
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body',)
+        labels = {
+            'body': (''),
+        }
+
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'slug', 'content', 'featured_image', 'excerpt')
+        lables = {
+            'title': _(''), 'slug': _(''), 'content': _(''), 'featured_image': _(''), 'excerpt': _(''),
+        }
