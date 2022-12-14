823a1ef7f810e834a6c84b804f32e87ec38a9926
eoghankb <eoghankb@gmail.com>
2022-12-14 22:23:14 +0000

    added blog like create and delete url paths

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,12 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-   path('', views.PostList.as_view(), name='home')
+    path('', views.PostList.as_view(), name='home'),
+    path('blog/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('blog/<int:blog_post_id>/',
+         views.PostDetail.as_view(), name='blog_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('blog/create', views.create_post, name='create_post'),
+    path('blog/delete/<int:blog_post_id>',
+         views.delete_post, name='delete_post'),
 ]
