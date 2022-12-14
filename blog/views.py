6932a754d961d5375b6ba0e0aff2ce992fe61b3f
eoghankb <eoghankb@gmail.com>
2022-12-14 22:24:25 +0000

    added views for post detail post post like create post and delete post

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views.generic import ListView, DetailView
+from django.contrib.auth.decorators import login_required
 from .models import Post
+from django.views.generic import (
+    View,)
+from .forms import CommentForm, BlogPostForm
 
 # Create your views here.
 
@@ -8,5 +12,125 @@ from .models import Post
 class PostList(ListView):
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "base.html"
+    template_name = "index.html"
     paginate_by = 6
+
+
+class PostDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": False,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
+
+
+def post(self, request, slug, *args, **kwargs):
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.filter(approved=True).order_by("-created_on")
+    liked = False
+    if post.likes.filter(id=self.request.user.id).exists():
+        liked = True
+
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        form = form.save(commit=False)
+        form.name = self.request.user
+        form.email = request.user.email
+        form.post = post
+        form.save()
+    else:
+        form = CommentForm()
+
+    return render(
+        request,
+        "post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "commented": True,
+            "form": form,
+            "liked": liked,
+            "comment_form": CommentForm()
+        },
+    )
+
+    def form_valid(self, form):
+        """ validate form and connect to user """
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+
+class PostLike(View):
+
+    def post(self, request, slug, *args, **kwargs):
+        post = get_object_or_404(Post, slug=slug)
+
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+
+
+@login_required
+def create_post(request):
+    """
+    Allow an admin user to create a Blog Post
+    """
+    if request.user:
+
+        if request.method == 'POST':
+            form = BlogPostForm(request.POST, request.FILES)
+            if form.is_valid():
+                blog_post = form.save(commit=False)
+                blog_post.author = request.user
+                blog_post.save()
+                messages.info(request, 'Blog Pending Approval!')
+                return redirect('/')
+            else:
+                messages.error(request, 'Please check the form for errors. \
+                    Blog failed to add.')
+        else:
+            form = BlogPostForm()
+    else:
+        messages.error(
+            request, 'Sorry, you do not have permission to do that.')
+        return redirect(reverse('home'))
+
+    template = 'new_post.html'
+
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
+
+
+@login_required
+def delete_post(request, blog_post_id):
+    """User can delete their own blog post"""
+    if request.method == "POST":
+        blog_post = get_object_or_404(Post, pk=blog_post_id)
+        blog_post.delete()
+    else:
+        return render(request, 'delete_blog.html')
+    messages.success(request, 'The blog has been deleted successfully!')
+
+    return redirect('/')
