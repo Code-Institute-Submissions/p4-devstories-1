21995599e9c0ce78e0e24d82e0e7ee3ab10bbdef
eoghankb <eoghankb@gmail.com>
2022-12-14 20:50:44 +0000

    changed url path to PostList

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,6 @@
+from . import views
 from django.urls import path
-#from . import views
-from .views import HomeView
 
 urlpatterns = [
-    #path('', views.HomeView.as_view(), name='home'),
-    path('', HomeView.as_view(), name='base.html'),
+   path('', views.PostList.as_view(), name='home')
 ]
