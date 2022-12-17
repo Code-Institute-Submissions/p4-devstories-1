189cd8eec29af76d6515423b1fe70d4ab028fa48
eoghankb <eoghankb@gmail.com>
2022-12-17 00:33:58 +0000

    updated url path for create blog to blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
+    path('blog/', views.create, name='new_post'),
     path('blog/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('blog/<int:blog_post_id>/',
          views.PostDetail.as_view(), name='blog_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('blog/create/', views.create_post, name='create_post'),
     path('blog/delete/<int:blog_post_id>',
          views.delete_post, name='delete_post'),
 ]
