9bb7072427cd83647aadd63e502686ad832df813
eoghankb <eoghankb@gmail.com>
2022-12-17 00:34:12 +0000

    add import for send mail

--- blog/views.py
+++ blog/views.py
@@ -11,8 +11,7 @@ from django.template import loader
 from .models import Post, Comment
 from .forms import CommentForm, BlogPostForm
 from django.contrib import messages
-
-# Create your views here.
+from django.core.mail import send_mail, BadHeaderError
 
 
 class PostList(ListView):
@@ -44,43 +43,71 @@ class PostDetail(View):
             },
         )
 
+    def post(self, request, slug, *args, **kwargs):
 
-def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
 
-    queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
-    comments = post.comments.filter(approved=True).order_by("-created_on")
-    liked = False
-    if post.likes.filter(id=self.request.user.id).exists():
-        liked = True
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.name = self.request.user
+            form.email = request.user.email
+            form.post = post
+            form.save()
+        else:
+            form = CommentForm()
 
-    form = CommentForm(data=request.POST)
-    if form.is_valid():
-        form = form.save(commit=False)
-        form.name = self.request.user
-        form.email = request.user.email
-        form.post = post
-        form.save()
-    else:
-        form = CommentForm()
-
-    return render(
-        request,
-        "post_detail.html",
-        {
-            "post": post,
-            "comments": comments,
-            "commented": True,
-            "form": form,
-            "liked": liked,
-            "comment_form": CommentForm()
-        },
-    )
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "form": form,
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
 
     def form_valid(self, form):
         """ validate form and connect to user """
         form.instance.created_by = self.request.user
         return super().form_valid(form)
+
+
+class CommentUpdateView(UpdateView):
+    """ Update comments via update_post.html """
+    model = Comment
+    form_class = CommentForm
+    context_object_name = 'comment'
+    template_name = 'update_post.html'
+
+    def form_valid(self, form):
+        """
+        Success url return to blogpost in question
+        with successfull commentform
+        """
+        self.success_url = f'/{self.get_object().post.slug}/'
+        return super().form_valid(form)
+
+
+class CommentDeleteView(DeleteView):
+    """ Connects comment to DeleteView function """
+    model = Comment
+    template_name = 'delete_comment_post.html'
+    context_object_name = 'comment'
+
+    def get_success_url(self, *args):
+        """ Success url return to blogpost in question """
+        self.success_url = f'/{self.get_object().post.slug}'
+        self.slug = self.get_object().post.slug
+        return reverse_lazy('post_detail', args=[self.slug])
 
 
 class PostLike(View):
@@ -97,7 +124,7 @@ class PostLike(View):
 
 
 @login_required
-def create_post(request):
+def create(request):
     """
     Allow an admin user to create a Blog Post
     """
@@ -129,6 +156,41 @@ def create_post(request):
     return render(request, template, context)
 
 
+# @login_required
+# def edit_blog(request, blog_post_id):
+#     """
+#     Allow all users to edit the blogs they created
+#     """
+#     if request.user:
+
+#         blog_post = get_object_or_404(Post, pk=blog_post_id)
+
+#         if request.method == 'POST':
+#             form = PostForm(request.POST, request.FILES, instance=blog_post)
+#             if form.is_valid():
+#                 form.save()
+#                 messages.success(request, 'Blog post updated successfully!')
+#                 return redirect('blog')
+#             else:
+#                 messages.error(request, 'Please check the form for errors. \
+#                     Blog post failed to update.')
+#         else:
+#             form = PostForm(instance=blog_post)
+#             messages.info(request, f'Editing {blog_post.title}')
+#     else:
+#         messages.error(request, 'Sorry, you do not have permission for that.')
+#         return redirect(reverse('home'))
+
+#     template = 'edit_blog.html'
+
+#     context = {
+#         'form': form,
+#         'blog_post': blog_post,
+#     }
+
+#     return render(request, template, context)
+
+
 @login_required
 def delete_post(request, blog_post_id):
     """User can delete their own blog post"""
@@ -140,32 +202,3 @@ def delete_post(request, blog_post_id):
     messages.success(request, 'The blog has been deleted successfully!')
 
     return redirect('/')
-
-
-class CommentUpdateView(UpdateView):
-    """ Update comments via update_post.html """
-    model = Comment
-    form_class = CommentForm
-    context_object_name = 'comment'
-    template_name = 'update_post.html'
-
-    def form_valid(self, form):
-        """
-        Success url return to blogpost in question
-        with successfull commentform
-        """
-        self.success_url = f'/{self.get_object().post.slug}/'
-        return super().form_valid(form)
-
-
-class CommentDeleteView(DeleteView):
-    """ Connects comment to DeleteView function """
-    model = Comment
-    template_name = 'delete_comment_post.html'
-    context_object_name = 'comment'
-
-    def get_success_url(self, *args):
-        """ Success url return to blogpost in question """
-        self.success_url = f'/{self.get_object().post.slug}'
-        self.slug = self.get_object().post.slug
-        return reverse_lazy('post_detail', args=[self.slug])
