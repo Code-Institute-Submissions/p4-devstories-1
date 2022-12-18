477446f293f69b403ce87a109c0334734955ba60
eoghankb <eoghankb@gmail.com>
2022-12-18 17:18:43 +0000

    changed url path from new post to create post

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('blog/', views.create, name='new_post'),
+    path('blog/create', views.create_post, name='create_post'),
     path('blog/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('blog/<int:blog_post_id>/',
          views.PostDetail.as_view(), name='blog_detail'),
