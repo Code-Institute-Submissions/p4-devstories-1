4e1c74ad4ecd18e8f5ac26fd884ed866a7505b81
eoghankb <eoghankb@gmail.com>
2022-12-18 17:51:46 +0000

    fix debug for os environ

--- devstories/settings.py
+++ devstories/settings.py
@@ -22,13 +22,14 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/ 
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = "DEVELOPMENT" in os.environ
+
 
 ALLOWED_HOSTS = ["app-devstories.herokuapp.com", "localhost"]
 
