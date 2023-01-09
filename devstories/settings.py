b32f286e91f54fd59749c4a82a4a880e0524f396
eoghankb <eoghankb@gmail.com>
2023-01-09 10:56:57 +0000

    fix debug setting

--- devstories/settings.py
+++ devstories/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = "DEVELOPMENT" in os.environ
-DEBUG = False
+DEBUG = os.environ.get('DEVENVIRON')
 
 
 ALLOWED_HOSTS = ["app-devstories.herokuapp.com", "localhost"]
